6b2e731347e8d7b71a627fd4dd4f9c33b667792c
Seth Mabbott <seth.mabbott@gmail.com>
2015-02-26 15:33:02 -0600

    use remote ip

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -2,9 +2,10 @@ class PusherController < ApplicationController
   protect_from_forgery :except => :auth # stop rails CSRF protection for this action
 
   def auth
+    debugger
     puts "-------------- PusherController"
     puts 'auth'
-    @current_ip = Ip.find_or_create_by(address: request.ip)
+    @current_ip = Ip.find_or_create_by(address: request.remote_ip)
     # @ips = Ip.all
     response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
       :user_id => @current_ip.id,
