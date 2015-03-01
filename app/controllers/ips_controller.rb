7a111ae8831d18c50dc618866c064d3682102b0e
Seth Mabbott <seth.mabbott@gmail.com>
2015-02-28 22:42:21 -0600

    working on authenticating pusher webhook requests

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -1,9 +1,14 @@
 class IpsController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, only: :presence_event
   
   def presence_event
     puts "----------------------------------"
     puts "catch presence event"
     # TODO: create an ip or user given pusher subscription webhook
+
+    puts params
+    render nothing: true, status: 200
   end
 
   def delete
