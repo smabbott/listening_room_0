a1f1873310d4433e52e33a80388af8254e964436
Seth Mabbott <seth.mabbott@gmail.com>
2015-03-10 23:36:59 -0500

    vary tempo

--- app/models/voice.rb
+++ app/models/voice.rb
@@ -27,7 +27,7 @@ class Voice < ActiveRecord::Base
     self.carrier = FREQUENCIES[(rand * FREQUENCIES.length).round]
     self.fm = FREQUENCIES[((rand * FREQUENCIES.length)/2).round]
     self.am = FREQUENCIES[(rand * FREQUENCIES.length).round]
-    self.tempo = rand * (10000 / 1000) + 1000
+    self.tempo = (rand * (5000 - 1000)) + 1000
   end
 
 end
