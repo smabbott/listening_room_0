28aedf6ac3c2eec9fd9c39da3d9a310d447225aa
Seth Mabbott <seth.mabbott@gmail.com>
2015-03-22 10:55:51 -0500

    don't need to save voice object to db if we use timestaps as ids

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -4,11 +4,12 @@ class PusherController < ApplicationController
   def auth
     puts "-------------- PusherController"
     puts 'auth'
-    @current_voice = Voice.create(address:request.remote_ip)
+    @current_voice = Voice.new(address:request.remote_ip)
     voice = @current_voice.attributes.reject{|k,v| k.to_sym == :address}
     voice[:digits] = @current_voice.digits
+    voice[:id] = @current_voice.created_at.to_i
     response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
-      :user_id => @current_voice.id,
+      :user_id => @current_voice.created_at.to_i,
       :user_info => {
         :voice => voice,
       }
